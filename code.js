1003ba82ce2337b876f5ab167a579c5121801afb
DancingPotato <herobrinesayeh@gmail.com>
2023-03-18 03:08:06 +0200

    Fix grid grab

--- code.js
+++ code.js
@@ -5,30 +5,62 @@ let gridDimensions;*/
 //Y =X
 let grid;
 let color;
-let gridSize;
+let gridSize = 16;
+let gridDimensions = 500 / gridSize;
 const gridContainer = document.querySelector(".gridContainer");
 const colorPicker = document.querySelector(".colorPicker");
 const gridSizeBtn = document.querySelector(".gridSizeButton");
+const clearBtn = document.querySelector(".clear");
 
 colorPicker.addEventListener("change", (e) => {
     color = e.target.value;
     colorPicker.style.backgroundColor = `${color}`;
 });
 gridSizeBtn.addEventListener("click", () => { 
-    gridSize = prompt("Enter grid size")
+    gridSize = prompt("Enter grid size");
+
+    if (gridSize > 100 || gridSize < 1) {
+        alert("ERROR: Size should be between 1 to 100");
+        return;
+    }
+    gridDimensions = 500 / gridSize;
+    clearGrid();
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        grid = document.createElement("div");
+        grid.className = "grid";
+        grid.style.width = `${gridDimensions}px`;
+        grid.style.height = `${gridDimensions}px`;
+        grid.addEventListener('dragenter', (e) => e.target.style.backgroundColor = `${color}`);
+        grid.addEventListener('mousedown', (e) => e.target.style.backgroundColor = `${color}`);
+        gridContainer.appendChild(grid);
+        
+    }
+});
+clearBtn.addEventListener('click', () => {
+    while (gridContainer.firstChild.style.backgroundColor == 'white') {
+        gridContainer.lastChild.style.backgroundColor = 'white';
+    }
 });
 
 color = colorPicker.value;
 colorPicker.style.backgroundColor = `${color}`;
 
 //Grid drawer
-for (let i = 0; i < (16 * 16); i++) {
-    grid = document.createElement("div");
-    grid.className = "grid";
-    grid.addEventListener('dragenter', (e) => e.target.style.backgroundColor = `${color}`);
-    grid.addEventListener('mousedown', (e) => e.target.style.backgroundColor = `${color}`);
-    gridContainer.appendChild(grid);
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        grid = document.createElement("div");
+        grid.style.width = `${gridDimensions}px`;
+        grid.style.height = `${gridDimensions}px`;
+        grid.className = "grid";
+        grid.addEventListener('dragenter', (e) => e.target.style.backgroundColor = `${color}`);
+        grid.addEventListener('mousedown', (e) => e.target.style.backgroundColor = `${color}`);
+        gridContainer.appendChild(grid);
+    }
+
+function clearGrid() {
+    console.log(grid.style.backgroundColor);
 }
+
+
 
 
 /*grid.style.width = gridDimensions;
